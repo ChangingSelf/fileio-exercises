33edfaaf5831e809faf18741030c03fdabaf36af
ChangingSelf <ChangingSelf@163.com>
2020-02-29 22:29:35 +0800

    菜单：修改学生信息

--- src/view/ConsoleMenu.java
+++ src/view/ConsoleMenu.java
@@ -1,7 +1,9 @@
 package view;
 
 import java.util.Scanner;
+import java.util.Vector;
 
+import model.Gender;
 import model.StudentInfo;
 import model.StudentInfoSystem;
 
@@ -24,22 +26,24 @@ public class ConsoleMenu {
 				printList();
 				break;
 			case "B":case "b"://录入学生信息
+				System.out.println("====录入学生信息====");
 				addStudent();
 				break;
 			case "C":case "c"://查询学生信息
+				System.out.println("====查询学生信息====");
 				search();
 				break;
 			case "D":case "d"://修改学生信息
-				studentInfoSystem.printStudentList();
+				modify();
 				break;
 			case "E":case "e"://删除学生信息
-				studentInfoSystem.printStudentList();
+				remove();
 				break;
 			case "F":case "f"://将当前信息列表保存到文件
-				studentInfoSystem.printStudentList();
+				save();
 				break;
 			case "G":case "g"://从已有文件读取数据
-				studentInfoSystem.printStudentList();
+				load();
 				break;
 			case "H":case "h"://显示菜单
 				printMenu();
@@ -79,16 +83,17 @@ public class ConsoleMenu {
 	 * 录入学生信息
 	 */
 	public void addStudent() {
-		System.out.println("====录入学生信息====");
+		
 		StudentInfo newStudentInfo = studentInfoSystem.inputStudentInfo();
 		studentInfoSystem.addStudent(newStudentInfo);
 	}
 	
 	/**
 	 * 查找
+	 * @return 返回查找结果，如果未找到则返回null
 	 */
-	public void search() {
-		System.out.println("====查询学生信息====");
+	public StudentInfo search() {
+		
 		System.out.println("请输入学号");
 		Scanner scanner = new Scanner(System.in);
 		String studentId = scanner.next();
@@ -100,10 +105,102 @@ public class ConsoleMenu {
 		}
 		else {
 			studentInfo.printInfo(true);//如果找到了则输出
+			
 		}
+		return studentInfo;
 	}
 	
+	/**
+	 * 修改学生信息
+	 * @return 是否修改成功
+	 */
+	public boolean modify() {
+		printList();
+		System.out.println("====修改学生信息====");
+		StudentInfo studentInfo = search();
+		if(studentInfo == null) return false;
+		System.out.println("请依次输入修改后的内容");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("请输入学生的学号，如果不需要修改，则直接回车");
+		String studentId = scanner.nextLine();
+		studentId = (studentId.equals(""))?null:studentId;
+		
+		
+		System.out.println("请输入学生的姓名，如果不需要修改，则直接回车");
+		String name = scanner.nextLine();
+		name = (name.equals(""))?null:name;
+		
+		String genderString = "";
+		Gender gender = null;
+		System.out.println("请输入学生的性别,输入\"男\" 或者\"女\"，输入别的内容则不修改");
+		genderString = scanner.nextLine();
+		gender =  Gender.newGender(genderString);
+		
+		
+		System.out.println("请输入学生的年龄，如果不需要修改，则输入0或者其他不合法数据");
+		int age = 0;
+		
+		try {
+			age = scanner.nextInt();
+		} catch (Exception e) {
+			scanner.next();//清空错误数据
+			age = 0;
+		}
+		
+		if(age<0) age = 0;
 
+		scanner.nextLine();//清除缓冲区空行
+		
+		System.out.println("请输入学生的专业，如果不需要修改，则直接回车");
+		String major = scanner.nextLine();
+		major = (major.equals(""))?null:major;
+		
+		
+		System.out.println("请输入学生的奖励，每输入完一项换行，输入\"done\"结束输入，输入\"null\"表示不需要修改");
+		String rewardString = "";
+		Vector<String> reward = new Vector<String>();
+		do {
+			rewardString = scanner.nextLine();
+			if(!rewardString.equalsIgnoreCase("done") && !rewardString.equalsIgnoreCase("null")) {
+				reward.add(rewardString);
+			}
+				
+		} while (!rewardString.equalsIgnoreCase("done") && !rewardString.equalsIgnoreCase("null"));
+		if(rewardString.equalsIgnoreCase("null")) {
+			reward = null;//不需要修改
+		}
+		
+		
+		return studentInfoSystem.modify(studentInfo, studentId, name, gender, age, major, reward);
+		
+	}
+	
+	public void remove() {
+		
+	}
+
+	public void save() {
+		studentInfoSystem.saveData(".");
+	}
+	
+	/**
+	 * 
+	 * @return 是否加载成功
+	 */
+	public boolean load() {
+		int studentNum = studentInfoSystem.loadData(".", true);
+		if(studentNum == -1)
+		{
+			System.out.println("读取失败，请检查路径是否正确，以及文件是否符合格式");
+			return false;
+		}
+		
+		System.out.printf("成功载入%d条学生信息\n",studentNum);
+		return true;
+		
+	}
+	
 	public static void main(String[] args) {
 		ConsoleMenu menu = new ConsoleMenu();
 		menu.printMenu();
